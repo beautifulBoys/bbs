3417eb25820c713d2c2c97a66f70f4b85238fd4e
beautifulBoy <1298947916@qq.com>
2017-06-09 18:06:22 +0800

    写首页

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,18 +5,29 @@ import React from 'react';
 
 export default class Footer extends React.Component {
   render() {
+    var links = [
+      {id: 0, text: '常用网站', link: '#'},
+      {id: 1, text: '相关下载', link: '#'},
+      {id: 2, text: '未名站刊', link: '#'},
+      {id: 3, text: '关于我们', link: '#'},
+      {id: 4, text: '联系我们', link: '#'},
+      {id: 5, text: '加入我们', link: '#'},
+      {id: 6, text: '帮助中心', link: '#'},
+      {id: 7, text: '意见反馈', link: '#'}
+    ];
     return (
       <div className="footer">
         <div className="footer-main">
           <p>
-            <a href="#">常用网站</a> -
-            <a href="#">相关下载</a> -
-            <a href="#">未名站刊</a> -
-            <a href="#">关于我们</a> -
-            <a href="#">联系我们</a> -
-            <a href="#">加入我们</a> -
-            <a href="#">帮助中心</a> -
-            <a href="#">意见反馈</a>
+            {
+              links.map((item) => {
+                if (links.length - 1 !== item.id) {
+                  return <div key={item.id}><a href={item.link}>{item.text}</a><span> -</span> </div>
+                } else {
+                  return <div key={item.id}><a href={item.link}>{item.text}</a></div>
+                }
+              })
+            }
           </p>
           <p>© 2000–2017 北大未名BBS</p>
         </div>
--- src/views/main.js
+++ src/views/main.js
@@ -5,11 +5,14 @@ import React from 'react';
 import Header from '../components/header.js';
 import Footer from '../components/footer.js';
 
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Pagination } from 'antd';
 
 export default class AppComponent extends React.Component {
   render() {
     var a = [{id: 1, name: '中国矿业大学', link: '#'}, {id: 2, name: '新能源工程学院'}, {id: 3, name: '信呢各院', link: '#'}, {id: 4, name: '张建英'}];
+    var onChange = function (pageNumber) {
+      console.log('Page: ', pageNumber);
+    };
     return (
       <div className="main-body">
         <Header/>
@@ -29,7 +32,10 @@ export default class AppComponent extends React.Component {
             </Breadcrumb>
           </div>
 
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <div className="content-header">
+            <Pagination showQuickJumper defaultCurrent={2} total={50} onChange={this.onChange} />
+          </div>
+
 
 
         </div>
